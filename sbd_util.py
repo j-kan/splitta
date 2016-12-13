114fcb969edc1b7917bb89dc8a0e6b89933f7b2f
J Kan <jim@intangible-industries.com>
2016-12-13 06:14:06 -0500

    works with python3

--- sbd_util.py
+++ sbd_util.py
@@ -1,21 +1,21 @@
-import re, cPickle, os, gzip, sys, math
+import re, pickle, os, gzip, sys, math
 
 def save_pickle(data, path):
     o = gzip.open(path, 'wb')
-    cPickle.dump(data, o)
+    pickle.dump(data, o)
     o.close()
 
 ZCAT = 'gzcat' if 'Darwin' in os.popen('uname -a').read().split() else 'zcat'
 
 def load_pickle(path):
-    #i = gzip.open(path, 'rb')
-    i = os.popen(ZCAT + ' ' + path)
-    data = cPickle.load(i)
+    i = gzip.open(path, 'rb')
+    #i = os.popen(ZCAT + ' ' + path)
+    data = pickle.load(i)
     i.close()
     return data
 
 def die(msg):
-    print '\nERROR: %s' %msg
+    print('\nERROR: %s' %msg)
     sys.exit()
 
 def logit(x, y=1):
@@ -26,7 +26,7 @@ def get_files(path, pattern):
     Recursively find all files rooted in <path> that match the regexp <pattern>
     """
     L = []
-    
+
     # base case: path is just a file
     if (re.match(pattern, os.path.basename(path)) != None) and os.path.isfile(path):
         L.append(path)
@@ -96,7 +96,7 @@ class Counter(dict):
        """
        for key, value in self.items():
            s = str(key) + ': ' + str(value)
-           print s
+           print(s)
 
    def displaySorted(self, N=10):
        """
@@ -105,7 +105,7 @@ class Counter(dict):
        sortedKeys = self.sortedKeys()
        for key in sortedKeys[:N]:
            s = str(key) + ': ' + str(self[key])
-           print s
+           print(s)
 
 def normalize(counter):
    """
